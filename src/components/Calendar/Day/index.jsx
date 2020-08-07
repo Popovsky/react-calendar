1cbdf03ed29d3a93783a7fa14e460e6c80c4b427
pad19 <pad19051999@gmail.com>
2020-08-07 16:48:43 +0300

    Fix all

--- src/components/Calendar/Day/index.jsx
+++ src/components/Calendar/Day/index.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Day.module.scss';
-import {getDate, format} from 'date-fns';
+import {format} from 'date-fns';
 
-function Day(props) {
-    const {date} = props;
+const Day = ({currentDate}) => {
     return (
         <div className={styles.wrapper}>
-            <div className={styles.name}>{format(date, 'iiii')}</div>
-            <div className={styles.number}>{getDate(date)}</div>
+            <div className={styles.name}>{format(currentDate, 'iiii')}</div>
+            <div className={styles.number}>{format(currentDate, 'd')}</div>
         </div>
     );
 }
 
 Day.propTypes = {
-    date: PropTypes.instanceOf(Date).isRequired,
+    currentDate: PropTypes.instanceOf(Date).isRequired,
 }
 
 export default Day;
--- src/components/Calendar/Month/Week/index.jsx
+++ src/components/Calendar/Month/Week/index.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Day from './Date';
+import CalendarDate from './CalendarDate';
 import styles from './Week.module.scss';
-import {getDate, setDate} from 'date-fns';
+import {eachDayOfInterval, startOfWeek, endOfWeek} from 'date-fns';
 
-const getWeek = ({currentDate, date}) => {
-    const days = [];
-    for (let i = 0; i < 7; i++) {
-        const singleDate = setDate(date, getDate(date) + i);
-        days.push(<Day key={singleDate} date={singleDate} currentDate={currentDate}/>);
-    }
-    return days;
-}
+const getWeekDays = ({firstDayOfWeek, weekDate, date, currentDate}) => eachDayOfInterval({
+    start: startOfWeek(weekDate, {weekStartsOn: firstDayOfWeek}),
+    end: endOfWeek(weekDate, {weekStartsOn: firstDayOfWeek}),
+}).map(dayDate => <CalendarDate key={dayDate} dayDate={dayDate} date={date} currentDate={currentDate}/>);
 
-function Week(props) {
-    const {currentDate, date} = props;
-    return (
-        <ul className={styles.row}>{getWeek({currentDate, date})}</ul>
-    );
-}
+const Week = props => <ul className={styles.row}>{getWeekDays(props)}</ul>;
 
 Week.propTypes = {
+    weekDate: PropTypes.instanceOf(Date).isRequired,
     date: PropTypes.instanceOf(Date).isRequired,
-    currentDate: PropTypes.instanceOf(Date).isRequired,
+    currentDate: PropTypes.instanceOf(Date),
+    firstDayOfWeek: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6]),
+}
+
+Week.defaultProps = {
+    currentDate: new Date(),
+    firstDayOfWeek: 0,
 }
 
 export default Week;
